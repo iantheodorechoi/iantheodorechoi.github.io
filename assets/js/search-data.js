f9538691f4b1937e59c8b08cc1bb7f79144df857
iantheodorechoi <itcitcitc9887@gmail.com>
2025-10-05 08:08:11 +0000

    Deploying to gh-pages from @ iantheodorechoi/iantheodorechoi.github.io@b87d8670dc5e7296ad95c417210aa9a389b837d5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,17 +18,10 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Click the CV icon on the right to view my CV.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
-          },
-        },{id: "nav-submenus",
-          title: "submenus",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/_pages/dropdown/";
           },
         },{id: "post-a-post-with-plotly-js",
         
